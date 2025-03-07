3d7fbdd13f4e0e9846b77cc24cba7b94947e0a41
LTilio <leandro.tilio@gmail.com>
2025-03-07 16:02:20 -0300

    Feat: Config swagger token jwt

--- src/main/java/br/com/servicos_auto/configs/SwaggerConfig.java
+++ src/main/java/br/com/servicos_auto/configs/SwaggerConfig.java
@@ -4,7 +4,15 @@ import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
 @Configuration
+@SecurityScheme(name = "bearerAuth", // Nome do esquema de segurança
+        type = SecuritySchemeType.HTTP, // Tipo de autenticação (HTTP)
+        scheme = "bearer", // Esquema (Bearer token)
+        bearerFormat = "JWT" // Formato do token (JWT)
+)
 public class SwaggerConfig {
 
     @Bean
